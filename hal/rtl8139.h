44dc580820d66ac09c5238f1eee0b3577874f9ee
akrom <akrommuhammadyusuf@gmail.com>
2026-09-12 10:00:00 +0700

    hal: add rtl8139_has_packet() for non-blocking RX polling

--- hal/rtl8139.h
+++ hal/rtl8139.h
@@ -5,9 +5,11 @@
 
 int  rtl8139_init(void);
 int  rtl8139_is_active(void);
+int  rtl8139_has_packet(void);
 int  rtl8139_get_mac(uint8_t *mac_out);
 int  rtl8139_send_packet(const void *data, uint32_t len);
 int  rtl8139_receive_packet(void *buf, uint32_t max_len);
 void rtl8139_get_stats(uint32_t *rx_pkts, uint32_t *tx_pkts, uint32_t *rx_bytes, uint32_t *tx_bytes);
 
 #endif // AKRYON_HAL_RTL8139_H
+
